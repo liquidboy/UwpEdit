93875c06f5142d98d96024f32e2d36b653faf4fb
Phill Campbell <phill.campbell@dionach.com>
2016-11-09 15:47:05 +0000

    initial solution structure

---
+++ test/UwpEdit.Test/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace UwpEdit.Test
+{
+    public class UnitTest1
+    {
+        #region Public Methods
+
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+
+        #endregion Public Methods
+    }
+}
