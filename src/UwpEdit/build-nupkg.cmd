c7c307e19de667ab28a929ca736e8eb43e53f2f3
Phill Campbell <phill.campbell@dionach.com>
2016-12-22 15:07:33 +0000

    fix nuget packaging

--- src/UwpEdit/build-nupkg.cmd
+++ src/UwpEdit/build-nupkg.cmd
@@ -3,9 +3,7 @@ IF %ERRORLEVEL% NEQ 0 (
     ECHO Error: It appears that 'msbuild' is not available in this environment. Try using the VS command prompt.
 )
 
-msbuild "%~dp0UwpEdit.csproj" /v:m /maxcpucount /nr:false  /p:RunCodeAnalysis=false /p:Configuration=Release /p:Platform=ARM
-msbuild "%~dp0UwpEdit.csproj" /v:m /maxcpucount /nr:false  /p:RunCodeAnalysis=false /p:Configuration=Release /p:Platform=x86
-msbuild "%~dp0UwpEdit.csproj" /v:m /maxcpucount /nr:false  /p:RunCodeAnalysis=false /p:Configuration=Release /p:Platform=x64
+msbuild "%~dp0UwpEdit.csproj" /v:m /maxcpucount /nr:false  /p:RunCodeAnalysis=false /p:Configuration=Release /p:Platform=AnyCPU
 
 
 powershell -command "Start-BitsTransfer -Source 'http://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -Destination '%~dp0build\nuget.exe'"
